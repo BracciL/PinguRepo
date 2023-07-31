7dca8bd1b9be49f6d43373fa411c6767e941d414
BracciL <lorenzobracci@outlook.com>
2023-07-31 10:37:21 +0200

    pelvic commit

---
+++ PinguGaming/src/main/java/com/Noot/PinguGaming/PinguGamingApplication.java
@@ -0,0 +1,13 @@
+package com.Noot.PinguGaming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinguGamingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PinguGamingApplication.class, args);
+	}
+
+}
---
+++ PinguGaming/src/test/java/com/Noot/PinguGaming/PinguGamingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Noot.PinguGaming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PinguGamingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
