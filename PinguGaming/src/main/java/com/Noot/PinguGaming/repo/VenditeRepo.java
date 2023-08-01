4ed2a70a541559046a38dc5e7cfe3565dd423ab0
BracciL <lorenzobracci@outlook.com>
2023-08-01 11:28:36 +0200

    classi repo pungus

---
+++ PinguGaming/src/main/java/com/Noot/PinguGaming/repo/VenditeRepo.java
@@ -0,0 +1,9 @@
+package com.Noot.PinguGaming.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.Noot.PinguGaming.model.Vendite;
+
+public interface VenditeRepo  extends JpaRepository<Vendite, Integer> {
+
+}
---
+++ PinguGaming/src/main/java/com/Noot/PinguGaming/repo/adminRepo.java
@@ -0,0 +1,9 @@
+package com.Noot.PinguGaming.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.Noot.PinguGaming.model.admin;
+
+public interface adminRepo extends JpaRepository<admin, Integer> {
+
+}
